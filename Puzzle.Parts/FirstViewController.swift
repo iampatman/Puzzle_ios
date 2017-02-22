d060cf3580922f36a7d0e94128565fbbe50d00f7
Nguyen Trung <nguyentrung0904@gmail.com>
2017-02-22 20:45:57 +0800

    Initial Commit

---
+++ Puzzle.Parts/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Puzzle.Parts
+//
+//  Created by Nguyen Bui An Trung on 22/2/17.
+//  Copyright © 2017 Nguyen Bui An Trung. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Puzzle.Parts/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Puzzle.Parts
+//
+//  Created by Nguyen Bui An Trung on 22/2/17.
+//  Copyright © 2017 Nguyen Bui An Trung. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
