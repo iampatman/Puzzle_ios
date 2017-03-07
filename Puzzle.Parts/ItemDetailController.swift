5635683c5d51b6506298018392046f660fbcf74f
Nguyen Trung <nguyentrung0904@gmail.com>
2017-03-08 01:12:43 +0800

    Add image scroll view to item detail screen

--- Puzzle.Parts/ItemDetailController.swift
+++ Puzzle.Parts/ItemDetailController.swift
@@ -11,10 +11,24 @@ import UIKit
 class ItemDetailController: UITableViewController {
 	var item: SubscriptionItem?
 	
+	@IBOutlet weak var photoScrollView: UIScrollView!
 	@IBOutlet weak var textViewDescription: UITextView!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
+		let image = UIImage(named: "starbucks-logo")
+		let imageView = UIImageView(image: image)
+		let height = Int(photoScrollView.frame.height - 5)
+		imageView.frame = CGRect(x: 0, y: 0, width: height, height: height)
+		imageView.contentMode = .scaleAspectFit
+		photoScrollView.addSubview(imageView)
+		imageView.frame = CGRect(x: height * 1, y: 0, width: height, height: height)
+		photoScrollView.addSubview(imageView)
+		imageView.frame = CGRect(x: height * 2, y: 0, width: height, height: height)
+		photoScrollView.addSubview(imageView)
+		photoScrollView.addSubview(imageView)
+		photoScrollView.addSubview(imageView)
+		photoScrollView.contentSize = CGSize(width: height*5, height: height)
     }
 	
 	override func viewWillAppear(_ animated: Bool) {
