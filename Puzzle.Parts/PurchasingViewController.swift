10a56692d1279db8345bd6823c77702d15cfe0ca
Nguyen Trung <nguyentrung0904@gmail.com>
2017-03-08 00:48:04 +0800

    Designing purchasing view

--- Puzzle.Parts/PurchasingViewController.swift
+++ Puzzle.Parts/PurchasingViewController.swift
@@ -13,6 +13,8 @@ class PurchasingViewController: UITableViewController {
 	var user: User?
 	var item: SubscriptionItem?
 	
+	@IBOutlet weak var datePickerCell: UITableViewCell!
+	@IBOutlet weak var datePickerStartDate: UIDatePicker!
 	@IBOutlet weak var txtStartDate: UITextField!
 	@IBOutlet weak var txtEndDate: UITextField!
 	@IBOutlet weak var txtPrice: UITextField!
@@ -23,6 +25,7 @@ class PurchasingViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		txtStartDate.text = "Today"
+		datePickerStartDate.minimumDate = Date()
 		progressView = ProgressViewController(text: "")
 		progressView?.isHidden = true;
 		progressView?.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
@@ -92,5 +95,70 @@ class PurchasingViewController: UITableViewController {
 			completion(returnCode!)
 			
 		})
+	}
+	@IBAction func btnPurchaseTouched(_ sender: UIButton) {
+		let hidden = self.datePickerStartDate.isHidden
+		self.datePickerStartDate.isHidden = !hidden
+	}
+	
+	@IBOutlet weak var btnPurchase: UIButton!
+	
+	func sendSubscribingRequest(_ completion: @escaping (Int) -> Void){
+		guard let user = user, let item = item else {
+			return;
+		}
+		let selected_pricing_id = 1
+		let selected_discount_id = 1
+		let url = NetworkManager.API_URL + "/subscription/getdetailbyid"
+		var params = Parameters()
+		params["mobilePhone"] = user.mobilePhone
+		params["sessionID"] = user.sessionID
+		params["subscription_id"] = item.id
+		params["pricing_id"] = selected_pricing_id
+		params["discount_id"] = selected_discount_id
+		params["sig"] = ""
+		alamofireManager?.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).validate().responseJSON(completionHandler: { (response) in
+			guard (response.result.isSuccess == true) else {
+				completion(ReturnCode.FAILED)
+				return;
+			}
+			
+			guard let data = response.result.value as? [String:Any] else {
+				completion(ReturnCode.FAILED)
+				return;
+			}
+			
+			let returnCode = data["returnCode"] as? Int
+			guard returnCode == ReturnCode.SUCCESS else {
+				completion(returnCode!)
+				return
+			}
+		});
+		
+		
+		
+	}
+	var edittingStartDate = false
+
+}
+
+extension PurchasingViewController {
+	override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		if (indexPath.section == 0 && indexPath.row == 1){
+			if (edittingStartDate){
+				return 120
+			} else {
+				return 0
+			}
+		}
+		return 44;
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if (indexPath.section == 0 && indexPath.row == 0){
+			edittingStartDate = !edittingStartDate
+			self.tableView.reloadData()
+			
+		}
 	}
 }
